d7a7f19f71405f489397e35c6b57f87ea9155d92
Anna <gatlingtow@gmail.com>
2024-11-20 13:02:23 +0200

    Film counter

--- js/script.js
+++ js/script.js
@@ -1,6 +1,23 @@
 "use strict";
 
-const isCheched = true,
-      isClose = true; 
 
-console.log(isCheched && isClose);
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?');
+
+
+const personalMovieDB = {
+        count: numberOfFilms,
+        movies: {},
+        actors: {},
+        genres: [],
+        private: false
+    };
+
+
+const a = prompt('Один из последних посмотренных фильмов', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних посмотренных фильмов', ''),
+      d = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[a] = b; //Используем квадратные скобки вместо точки для избежания дальнейших багов
+personalMovieDB.movies[c] = d;
+console.log(personalMovieDB);
