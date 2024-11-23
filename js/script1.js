bc8a63d20a391addb3d1b57e9ba1faf229227cdf
Anna <gatlingtow@gmail.com>
2024-11-23 08:29:11 +0200

    conditions added

--- js/script1.js
+++ js/script1.js
@@ -1,28 +1,35 @@
 'use strict';
 
-let numberOfFilms = 0;
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: [],
-    genres: [],
-    private: false
-};
-
-for (let i = 0; i < 3; i++){
-    numberOfFilms += +prompt('Сколько фильмов вы уже посмотрели?');
-    const movie = prompt('Один из последних просмотренных фильмов', '');
-    const rating = prompt('На сколько оцените его?','');
+    for (let i = 0; i < 3; i++) {
+        let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+        const movie = prompt('Один из последних просмотренных фильмов', '');
+        const rating = prompt('На сколько оцените его?','');
     
-    if (numberOfFilms === '' || movie === '' || rating === '') {
-        alert ('Введите корректные данные');
-        i--;
-    } 
-    else {
-        personalMovieDB.movies[movie] = rating;
-    }
-};
 
-console.log(personalMovieDB);
+        if (numberOfFilms.length > 50 || movie.length > 50 || rating.length > 50) {
+            alert ('Слишком длинная строка');
+            i--;
+        }
+        else {
+            if (numberOfFilms === '' || movie === '' || rating === '') {
+                alert ('Введите корректные данные');
+                i--;
+            } 
+            else {
+                
+                const personalMovieDB = {
+                    count: numberOfFilms += numberOfFilms,
+                    movies: {},
+                    actors: [],
+                    genres: [],
+                    private: false
+                };
+                
+                personalMovieDB.movies[movie] = rating;
+                console.log(personalMovieDB);
+            }
+        };
+    
+    };
 
